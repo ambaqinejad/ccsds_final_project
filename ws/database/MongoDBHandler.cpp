7095523bcb85df331fa24e1d36b7866dc857ed34
ambaqinejad <ambaqinejad@gmail.com>
2025-12-01 18:16:11 +0330

    wip

--- ws/database/MongoDBHandler.cpp
+++ ws/database/MongoDBHandler.cpp
@@ -22,7 +22,7 @@ MongoDBHandler::MongoDBHandler() {
                                  Constants::MONGODB_DEFAULT_URI);
     LOG_INFO << "DATABASE URI -------> " << uri;
     client_ = mongocxx::client{mongocxx::uri{uri}};
-    database_ = client_["CCSDS_DB"];
+    database_ = client_[Constants::DEFAULT_DB_NAME];
 }
 
 
@@ -55,22 +55,22 @@ bool MongoDBHandler::insertPacket(const CCSDS_Packet &packet) {
 }
 
 void MongoDBHandler::insertHeader(bsoncxx::builder::basic::document &document, const CCSDS_Packet &packet) {
-    document.append(bsoncxx::builder::basic::kvp("main_frame_header", packet.main_frame_header));
-    document.append(bsoncxx::builder::basic::kvp("packet_id", packet.packet_id));
-    document.append(bsoncxx::builder::basic::kvp("packet_sequence_control", packet.packet_sequence_control));
-    document.append(bsoncxx::builder::basic::kvp("packet_length", packet.packet_length));
-    document.append(bsoncxx::builder::basic::kvp("data_field_header", static_cast<int>(packet.data_field_header)));
-    document.append(bsoncxx::builder::basic::kvp("service_type", static_cast<int>(packet.service_type)));
-    document.append(bsoncxx::builder::basic::kvp("sub_service_type", static_cast<int>(packet.sub_service_type)));
-    document.append(bsoncxx::builder::basic::kvp("sid", static_cast<int>(packet.sid)));
-    document.append(bsoncxx::builder::basic::kvp("timestamp", static_cast<int64_t>(packet.timestamp)));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_MAIN_FRAME_HEADER, packet.main_frame_header));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_PACKET_ID, packet.packet_id));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_PACKET_SEQUENCE_CONTROL, packet.packet_sequence_control));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_PACKET_LENGTH, packet.packet_length));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_DATA_FIELD_HEADER, static_cast<int>(packet.data_field_header)));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_SERVICE_TYPE, static_cast<int>(packet.service_type)));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_SUB_SERVICE_TYPE, static_cast<int>(packet.sub_service_type)));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_SID, static_cast<int>(packet.sid)));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_TIMESTAMP, static_cast<int64_t>(packet.timestamp)));
     document.append(
-            bsoncxx::builder::basic::kvp("crc_fail_upload_map", static_cast<int64_t>(packet.crc_fail_upload_map)));
-    document.append(bsoncxx::builder::basic::kvp("flash_address", static_cast<int>(packet.flash_address)));
+            bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_CRC_FAIL_UPLOAD_MAP, static_cast<int64_t>(packet.crc_fail_upload_map)));
+    document.append(bsoncxx::builder::basic::kvp(Constants::PACKET_HEADER_KEY_FLASH_ADDRESS, static_cast<int>(packet.flash_address)));
 }
 
 void MongoDBHandler::insertStructure(nlohmann::ordered_json json) {
-    mongocxx::collection collection = database_["CCSDS_Structure"];
+    mongocxx::collection collection = database_[Constants::DEFAULT_COLLECTION_NAME];
     collection.drop(); // Clear previous data
 
     int i = 1;
@@ -91,7 +91,7 @@ using bsoncxx::builder::stream::finalize;
 nlohmann::ordered_json MongoDBHandler::ccsds_structure_;
 bool MongoDBHandler::loadStructure() {
     mongocxx::collection history_collection;
-    history_collection = database_["CCSDS_STRUCTURE_HISTORY"];
+    history_collection = database_[Constants::DEFAULT_HISTORY_COLLECTION_NAME];
     auto filter = document{} << "is_current" << true << finalize;
     auto result = history_collection.find_one(filter.view());
     if (result && (*result)["collection_name"]) {
@@ -105,11 +105,11 @@ bool MongoDBHandler::loadStructure() {
             MongoDBHandler::ccsds_structure_.push_back(j);
         }
         if (!MongoDBHandler::ccsds_structure_.empty()) {
-            LOG_INFO << "Structure loaded successfully from DB to RAM!";
+            LOG_INFO << Constants::STRUCTURE_LOADED_FROM_DB_TO_RAM;
             return true;
         }
     }
-    LOG_INFO << "Structure could not load from DB to RAM!\n";
+    LOG_INFO << Constants::STRUCTURE_COULD_NOT_LOAD_FROM_DB_TO_RAM;
     return false;
 }
 
--- ws/helpers/Constants.h
+++ ws/helpers/Constants.h
@@ -17,9 +17,11 @@ public:
     inline static const std::string SERVER_COULD_NOT_START_STRUCTURE = "Websocket server could not start because structure did not load.";
     inline static const std::string SERVER_COULD_NOT_START_PUBLIC_DIR = "Websocket server could not start because it can not create public directory.";
     inline static const std::string SERVER_COULD_NOT_START_UPLOAD_DIR = "Websocket server could not start because it can not create upload directory.";
+    inline static const std::string STRUCTURE_COULD_NOT_LOAD_FROM_DB_TO_RAM = "Structure could not load from DB to RAM!";
 
     // SERVER INFO
     inline static const std::string SERVER_START_ON_PORT = "Starting WebSocket server on port ";
+    inline static const std::string STRUCTURE_LOADED_FROM_DB_TO_RAM = "Structure loaded successfully from DB to RAM!";
 
     // RESPONSE HEADER
     inline static const std::string ACCESS_CONTROL_ALLOW_ORIGIN_KEY = "Access-Control-Allow-Origin";
@@ -31,6 +33,23 @@ public:
 
     // MONGODB
     inline static const std::string MONGODB_DEFAULT_URI = "mongodb://192.168.102.94:27017";
+    inline static const std::string DEFAULT_DB_NAME = "CCSDS_DB";
+    inline static const std::string DEFAULT_COLLECTION_NAME = "CCSDS_Structure";
+    inline static const std::string DEFAULT_HISTORY_COLLECTION_NAME = "CCSDS_STRUCTURE_HISTORY";
+
+    // PACKET
+    inline static const std::string PACKET_HEADER_KEY_MAIN_FRAME_HEADER = "main_frame_header";
+    inline static const std::string PACKET_HEADER_KEY_PACKET_ID = "packet_id";
+    inline static const std::string PACKET_HEADER_KEY_PACKET_SEQUENCE_CONTROL = "packet_sequence_control";
+    inline static const std::string PACKET_HEADER_KEY_PACKET_LENGTH = "packet_length";
+    inline static const std::string PACKET_HEADER_KEY_DATA_FIELD_HEADER = "data_field_header";
+    inline static const std::string PACKET_HEADER_KEY_SERVICE_TYPE = "service_type";
+    inline static const std::string PACKET_HEADER_KEY_SUB_SERVICE_TYPE = "sub_service_type";
+    inline static const std::string PACKET_HEADER_KEY_SID = "sid";
+    inline static const std::string PACKET_HEADER_KEY_TIMESTAMP = "timestamp";
+    inline static const std::string PACKET_HEADER_KEY_CRC_FAIL_UPLOAD_MAP = "crc_fail_upload_map";
+    inline static const std::string PACKET_HEADER_KEY_FLASH_ADDRESS = "flash_address";
+
 
 };
 #endif //FINAL_PROJECT_CONSTANTS_H
